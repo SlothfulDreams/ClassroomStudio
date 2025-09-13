4e4570062a40d9cd8125ac17e055164f0bb5dad1
SlothfulDreams <bensonzheng87901@gmail.com>
2025-09-13 12:55:01 -0400

    init

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ConvexAuthNextjsServerProvider>
+      <html lang="en">
+        <body>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
+  );
+}
